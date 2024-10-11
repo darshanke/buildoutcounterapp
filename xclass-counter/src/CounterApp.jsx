1abd94fed97ad16c57a84ef479a6c9ffbdb8e75e
darshan <darshanke646@gmail.com>
2024-10-12 01:12:27 +0530

    class Based compnenet

---
+++ xclass-counter/src/CounterApp.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+   
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+ 
+  increment() {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+
+ 
+  decrement() {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
+
+  // Render method
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
